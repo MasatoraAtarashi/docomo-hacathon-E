45841f53e157224c099ef7b53abae53b0e973630
masatora <atarashi.masatora@gmail.com>
2020-09-18 11:08:41 +0900

    Add fishing community to all resions

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,19 @@ study_community = Community.create(
   url: 'https://discord.gg/uYebc84', 
   picture: 'https://bit.ly/35L30K1'
 )
+osanpo_community = Community.create(
+  name: 'お散歩コミュニティ', 
+  url: 'https://discord.gg/uYebc84', 
+  picture: 'https://bit.ly/3kqBs0M'
+)
 c1 = Category.create(name: '地域')
 
 kyusyu = Category.create(name: '九州', ancestor_id: c1.id)
 tyugoku = Category.create(name: '中国四国', ancestor_id: c1.id)
-Category.create(name: '近畿', ancestor_id: c1.id)
-Category.create(name: '中部', ancestor_id: c1.id)
+kinki = Category.create(name: '近畿', ancestor_id: c1.id)
+tyubu = Category.create(name: '中部', ancestor_id: c1.id)
 kanto = Category.create(name: '関東', ancestor_id: c1.id)
-Category.create(name: '東北北海道', ancestor_id: c1.id)
+hokkaido = Category.create(name: '東北北海道', ancestor_id: c1.id)
 
 c2 = Category.create(name: '趣味')
 
@@ -37,7 +42,7 @@ c3 = Category.create(name: '年齢')
 niju = Category.create(name: '20代中心', ancestor_id: c3.id)
 sanju = Category.create(name: '30代中心', ancestor_id: c3.id)
 yonju = Category.create(name: '40代中心', ancestor_id: c3.id)
-Category.create(name: 'その他', ancestor_id: c3.id)
+sonota = Category.create(name: 'その他', ancestor_id: c3.id)
 
 c4 = Category.create(name: '性別')
 man = Category.create(name: '男性中心', ancestor_id: c4.id)
@@ -58,7 +63,7 @@ CommunityCategory.create(community_id: community.id, category_id: sanju.id)
 CommunityCategory.create(community_id: community.id, category_id: girl.id)
 
 # 釣り 九州, アウトドア, 男性中心, 40大
-CommunityCategory.create(community_id: fishing_community.id, category_id: kyusyu.id)
+CommunityCategory.create(community_id: fishing_community.id, category_id: kanto.id)
 CommunityCategory.create(community_id: fishing_community.id, category_id: outdoor.id)
 CommunityCategory.create(community_id: fishing_community.id, category_id: man.id)
 CommunityCategory.create(community_id: fishing_community.id, category_id: yonju.id)
@@ -68,3 +73,22 @@ CommunityCategory.create(community_id: study_community.id, category_id: man.id)
 CommunityCategory.create(community_id: study_community.id, category_id: engineer.id)
 CommunityCategory.create(community_id: study_community.id, category_id: niju.id)
 CommunityCategory.create(community_id: study_community.id, category_id: kanto.id)
+
+# お散歩 関東, 女性, その他、アウトドア
+CommunityCategory.create(community_id: osanpo_community.id, category_id: sonota.id)
+CommunityCategory.create(community_id: osanpo_community.id, category_id: girl.id)
+CommunityCategory.create(community_id: osanpo_community.id, category_id: outdoor.id)
+CommunityCategory.create(community_id: osanpo_community.id, category_id: kanto.id)
+
+regions = [kyusyu, tyugoku, kinki, tyubu, kanto, hokkaido]
+regions.each do |region|
+  fishing_community_each_region = Community.create(
+    name: "釣りコミュニティ(#{region.name})", 
+    url: 'https://discord.gg/uYebc84', 
+    picture: 'https://bit.ly/32DAPe3'
+  )
+  CommunityCategory.create(community_id: fishing_community_each_region.id, category_id: region.id)
+  CommunityCategory.create(community_id: fishing_community_each_region.id, category_id: outdoor.id)
+  CommunityCategory.create(community_id: fishing_community_each_region.id, category_id: man.id)
+  CommunityCategory.create(community_id: fishing_community_each_region.id, category_id: yonju.id)
+end
