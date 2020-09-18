61e71e8964c34aa0756222171665c2d703b3e169
masatora <atarashi.masatora@gmail.com>
2020-09-18 10:46:49 +0900

    Deal with communities without images etc.

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -3,35 +3,36 @@ class CommunitiesController < ApplicationController
   end
 
   def create
-    community = Community.new(name: params[:name], url: params[:url])
-    if community.save
-      # category_names = params[:category].split(/\s/)
-      # category_names.each do |name|
-      #   category = Category.create(name: name)
-      #   CommunityCategory.create(community_id: community.id, category_id: category.id)
-      # end
-      region = Category.find_by(name: params[:region])
-      hobby = Category.find_by(name: params[:hobby])
-      age = Category.find_by(name: params[:age])
-      sex = Category.find_by(name: params[:sex])
-      job = Category.find_by(name: params[:job])
-      if region
-        CommunityCategory.create(community_id: community.id, category_id: region.id)
-      end
-      if hobby
-        CommunityCategory.create(community_id: community.id, category_id: hobby.id)
-      end
-      if age
-        CommunityCategory.create(community_id: community.id, category_id: age.id)
-      end
-      if sex
-        CommunityCategory.create(community_id: community.id, category_id: sex.id)
-      end
-      if job
-        CommunityCategory.create(community_id: community.id, category_id: job.id)
-      end
+    unless params[:name] && params[:url] && params[:picture]
+      flash.now[:danger] = "登録に失敗しました。name, url, pictureを入力してください"
+      render :new
     else
-
+      community = Community.new(name: params[:name], url: params[:url], picture: params[:picture])
+      if community.save
+        region = Category.find_by(name: params[:region])
+        hobby = Category.find_by(name: params[:hobby])
+        age = Category.find_by(name: params[:age])
+        sex = Category.find_by(name: params[:sex])
+        job = Category.find_by(name: params[:job])
+        if region
+          CommunityCategory.create(community_id: community.id, category_id: region.id)
+        end
+        if hobby
+          CommunityCategory.create(community_id: community.id, category_id: hobby.id)
+        end
+        if age
+          CommunityCategory.create(community_id: community.id, category_id: age.id)
+        end
+        if sex
+          CommunityCategory.create(community_id: community.id, category_id: sex.id)
+        end
+        if job
+          CommunityCategory.create(community_id: community.id, category_id: job.id)
+        end
+      else
+        flash.now[:danger] = "登録に失敗しました。name, url, pictureを入力してください"
+        render :new
+      end
     end
   end
 end
--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,8 @@
 class Community < ApplicationRecord
   has_many :community_categories
   has_many :categories, through: :community_categories
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :picture, presence: true
 end
